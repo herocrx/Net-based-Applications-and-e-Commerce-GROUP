23e723d5708a0769737ab3f238dede1596620d42
root <hubertkuc13@gmail.com>
2016-11-06 19:49:06 +0100

    The assignment1 is finished. Data is given through the parameter ( linux terminal)

--- client.cpp
+++ client.cpp
@@ -19,11 +19,11 @@ void error(const char *msg)
 
 class client{ 
     private:
-        char buffer[256];
+        char *buffer;
         bool UDPmessage;
-        char NameOfFile[];
+        char NameOfFile[256];
         char  *host_name;
-        int Port_Number;
+        char  *Port_Number;
         string IP_Client;
     public:
         bool SendUDPmessage();
@@ -35,56 +35,99 @@ class client{
 };
 
 bool client::ReceiveFile(){
-     int sockfd;
-     struct sockaddr_in serv_addr;
-     struct hostent *server;
-     server = gethostbyname(host_name);
-     if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     serv_addr.sin_family = AF_INET;
-     bcopy((char *)server->h_addr, 
-          (char *)&serv_addr.sin_addr.s_addr,
-          server->h_length);
-     serv_addr.sin_port = htons(Port_Number);
-     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");   fstream fs;
-     fs.open (NameOfFile, fstream::in | fstream::out | fstream::app);
-     if( fs.good() == true ){
-         cout << "There's an access to a file!" << endl;
-     }
-     else 
-        cout << "Access to the file is forbidden!"<< endl; 
+      int socketfd, newsockfd, portno;
+      socklen_t clilen;
+      char buffer[256];
+      struct sockaddr_in serv_addr, cli_addr;
+      int n;
+      socketfd = socket(AF_INET, SOCK_STREAM, 0);
+      if (socketfd < 0) 
+         error("ERROR opening socket");
+      bzero((char *) &serv_addr, sizeof(serv_addr));
+      portno = atoi(Port_Number);
+      serv_addr.sin_family = AF_INET;
+      serv_addr.sin_addr.s_addr = INADDR_ANY;
+      serv_addr.sin_port = htons(portno);
+      if (bind(socketfd, (struct sockaddr *) &serv_addr,
+               sizeof(serv_addr)) < 0) 
+               error("ERROR on binding");
+    long int length_of_file = 0;
+     int length_of_packet = 255;
+     int nr_received_packet = 1; 
+     listen(socketfd,5);
+     clilen = sizeof(cli_addr);
+     newsockfd = accept(socketfd, (struct sockaddr *) &cli_addr, &clilen);
+     if (newsockfd < 0) 
+          error("ERROR on accept");
+     fstream fs;
+     int Nr_Received_Bytes = -1;
+     int Loading_Previous = 0;
 
-    close(sockfd); 
+     double procent;
+     do{
+         int cnt = 0;
+         switch(nr_received_packet){
+            case 1:
+                cout <<"Create a program called: " <<NameOfFile << endl;
+                fs.open(NameOfFile,fstream::in | fstream::out | fstream::app);
+                if( fs.good() == true ){
+                   cout << "There's an access to a file!" << endl; 
+                }
+                bzero(buffer,256); 
+                n = read(newsockfd,buffer,256);
+                if (n < 0) error("ERROR reading from socket");  
+                length_of_file = atoi(buffer);
+                cout << "Size of a file: " << buffer << endl;
+                break;
+            default:   
+                bzero(buffer,length_of_packet); 
+                Nr_Received_Bytes += length_of_packet;
+                n = read(newsockfd,buffer,length_of_packet);
+                if (n < 0) error("ERROR reading from socket");  
+                if(Nr_Received_Bytes < length_of_file)
+                     fs.write(buffer,length_of_packet);
+                else{
+                    while(buffer[cnt]!='\0')
+                     cnt++;
+                    fs.write(buffer,cnt);
+                }
+                procent = double(Nr_Received_Bytes)/double(length_of_file)*100;
+                if(Loading_Previous != int(procent))
+                    cout << "|";
+                Loading_Previous = (int)procent;
+                break;
+     }
+    nr_received_packet++;
+} 
+while(Nr_Received_Bytes<length_of_file);    
+    cout << endl;
+    fs.close();
+    nr_received_packet = 1;
+    Nr_Received_Bytes = 0;
+    n = write(newsockfd,"I got your message",18);
+    if (n < 0) error("ERROR writing to socket");
+    close(newsockfd);
+    close(socketfd);
+    return true;
 }
 
-client::client(char *host_name, char *Port):host_name(host_name){
-   Port_Number = atoi(Port);
-   UDPmessage = true;      
+client::client(char *host_name,char*Port):host_name(host_name){
+   Port_Number = Port;
 }
 client::~client(){}
 
 bool client::EstablishConnection(){
+    return true;
 }
 
 void client::GetDataToServer(){
     cout << "Give name of the file: ";
-    cin >> NameOfFile;
-    cout <<endl << "Give port number: ;";
-    cin >> Port_Number;
-    IP_Client = "127.0.0.1";
+    cin >> NameOfFile;  
 }
 
 
 bool client::SendUDPmessage(){
     int sockfd;
-    int sockfd_new;
     struct sockaddr_in serv_addr;
     struct hostent *server;
     cout << host_name <<endl;
@@ -99,27 +142,18 @@ bool client::SendUDPmessage(){
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);
-    serv_addr.sin_port = htons(Port_Number);
-    //sockfd_new = connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr));
-       // error("ERROR connecting");
-     if(sendto(sockfd,&NameOfFile,sizeof(NameOfFile),0,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0){
+    serv_addr.sin_port = htons(2000);
+    string MSG = ',' + string(NameOfFile) + ',' + Port_Number + ',' + host_name + ','; 
+    char *Message = new char[MSG.length()];
+    for(unsigned int i = 0; i<MSG.length(); i++)
+        Message[i] = MSG[i];
+    cout << "UDP message is: " << Message << endl;
+     if(sendto(sockfd,Message,200,0,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0){
         printf("Error while sending the name of the file \n");
         UDPmessage = false;
      }
-     // Send Host name of Client 
-    if(sendto(sockfd,&Port_Number,sizeof(Port_Number),0,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0){
-        printf("Error while sending the host name\n");
-        UDPmessage = false;
-    }
-     // Send Port number of server socket
-    if(sendto(sockfd,&Port_Number,sizeof(Port_Number),0,(struct sockaddr *) &serv_addr,sizeof(serv_addr))<0){
-        printf("Error while sending the port number\n");
-        UDPmessage = false;
-    }
-    if(UDPmessage == false){
-        printf("Unable to set the connection with the server \n");
-    }
-
+     close(sockfd);
+     return true;
 }
 
 int main(int argc, char *argv[])
@@ -135,7 +169,7 @@ int main(int argc, char *argv[])
     client ClientNr1(argv[1],argv[2]);    
     ClientNr1.GetDataToServer();
     ClientNr1.SendUDPmessage();
-    //ClientNr1.ReceiveFile();
+    ClientNr1.ReceiveFile();
     ClientNr1.~client();
     return 0;    
 
--- server.cpp
+++ server.cpp
@@ -7,13 +7,11 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <iostream>
 #include <fstream>
 using namespace std;
-
-#define MAXBUFSIZE 65536
-
 void error(const char *msg)
 {
     perror(msg);
@@ -22,64 +20,160 @@ void error(const char *msg)
 
 class Server{
     private:  
-          int sockfd, newsockfd;
+          void AssignReceivedPacket();
           int PortNumber;
-          socklen_t serlen;
-          char *buffer;
-          struct sockaddr_in serv_addr, cli_addr;
+          char buffer[256];
           int n;
+          char NameOfFile[256];
+          int TcpPortConnection;
+          char ClientIp[256];
     public: 
-      Server(int PN):PortNumber(PN){}
+          Server(){
+             PortNumber = 2000;
+          }
           ~Server();
           bool ReceiveUDPinfo();
+          bool SendFile();
 };
 
-Server::~Server(){
-    cout << "Clean everything" << endl;
+void Server::AssignReceivedPacket(){
+    if(buffer[0] == ','){
+        int i = 1;
+        int counter =0;
+while(buffer[i] != ','){
+            NameOfFile[counter] = buffer[i] ;
+            i++;
+            counter++;
+        }
+        i++;
+        NameOfFile[counter] = '\0';
+        counter = 0;
+        char Port[256];
+        while(buffer[i] != ','){
+            Port[counter] = buffer[i];
+            i++;
+            counter++;
+        }
+        counter = 0;
+        TcpPortConnection = atoi(Port);
+        i++;
+        while(buffer[i] != ','){
+           ClientIp[counter] = buffer[i];
+           i++;
+           counter++;
+        }
+        ClientIp[counter] = '\0';
+        cout << "Received data:" << endl;
+        cout << "   Name of file: " << NameOfFile << endl;
+        cout << "   Port of TCP connection: " << TcpPortConnection << endl;
+        cout << "   Client IP address: " << ClientIp << endl;
+    }
 }
-bool Server::ReceiveUDPinfo(){
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+
+bool Server::SendFile(){
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    hostent *server;
+    char buffer[256];
+    portno = TcpPortConnection;
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
+    server = gethostbyname(ClientIp);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    bcopy((char *)server->h_addr,(char *)&serv_addr.sin_addr.s_addr,server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    fstream fs;
+    fs.open (NameOfFile, fstream::in | fstream::out | fstream::app);
+    if( fs.good() == true ){
+        cout << "There's an access to a file!" << endl;
+    }
+    else 
+        cout << "Access to the file is forbidden!"<< endl;
+    long int length = fs.tellg();
+    fs.seekg (0,ios::end);
+    length = fs.tellg() - length;
+    fs.seekg(0, ios::beg);  
+    const int size_of_packet = 255;
+    double procent;
+    int procent_previous;
+    int count_bits = 0;
+    bzero(buffer,256);
+    sprintf(buffer,"%ld", length);
+    write(sockfd,buffer,256);
+    while(length-size_of_packet>count_bits){
+        count_bits = count_bits + size_of_packet;
+        bzero(buffer,size_of_packet);
+        fs.read(buffer,size_of_packet); // each read the pointer shifts
+        n = write(sockfd,buffer,size_of_packet);
+        if (n < 0) 
+            error("ERROR writing to socket");
+        procent = double(count_bits)/double(length)*100;
+        if(int(procent) != procent_previous)
+            cout <<"|";
+         procent_previous = procent; 
+    }
+    bzero(buffer,size_of_packet);
+    fs.read(buffer,size_of_packet); // each read the pointer shifts
+    n = write(sockfd,buffer,length-count_bits);
+    if (n < 0) 
+        error("ERROR writing to socket");
+    fs.close();
+    bzero(buffer,255);
+    n = read(sockfd,buffer,255);
+    if (n < 0) 
+        error("ERROR reading from socket");
+    close(sockfd); 
+    return true;
+}
+
+
+Server::~Server(){
+   // cout << endl << "Clean everything" << endl;
+}
+
+bool Server::ReceiveUDPinfo(){
+    socklen_t serlen;
+    struct sockaddr_in serv_addr;
+    int sockfd;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0){     
+        cout <<"ERROR opening socket" << endl;
+        return false;
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(PortNumber);
     if (bind(sockfd, (struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-      error("ERROR on binding");
-  listen(sockfd,5);
+        cout << "ERROR on binding" << endl;
+    listen(sockfd,5);
     serlen = sizeof(serv_addr);
-    cout << "Wait for the message!" << endl;
     int status;
-    status = recvfrom(sockfd, buffer,MAXBUFSIZE, 0, (struct sockaddr *)&serv_addr,&serlen);
+    status = recvfrom(sockfd,buffer,255, 0, (struct sockaddr *)&serv_addr,&serlen);
     if (status < 0 )
         cout << "Cannot get an UDP message" << endl;
-     cout << "TWOJ BUFEREK: " << *buffer <<" rozmiar: "  << status <<  endl;
-     status = recvfrom(sockfd, buffer, MAXBUFSIZE, 0, (struct sockaddr *)&serv_addr,&serlen);
-    if (status < 0 )
-        cout << "Cannot get an UDP message" << endl;
-    cout << "TWOJ BUFEREK: " << *buffer << endl;
-      status = recvfrom(sockfd, buffer, MAXBUFSIZE, 0, (struct sockaddr *)&serv_addr,&serlen);
-    if (status < 0 )
-        cout << "Cannot get an UDP message" << endl;
-    cout << "TWOJ BUFEREK: " << buffer << endl;
-   return true;
+    AssignReceivedPacket();
+    close(sockfd); 
+    return true;
  }
 
 
 int main(int argc, char *argv[])
 {
-      if (argc < 2) {
-          fprintf(stderr,"ERROR, no port provided\n");
-          exit(1);
-      }
-      while(1){
+    while(1){
          fstream fs;
-         int Convert_Value = atoi(argv[1]);
-         cout << Convert_Value << endl;
-         Server Server1 = Server(Convert_Value);
+         Server Server1 = Server();
          Server1.ReceiveUDPinfo();
-         cout << "Wykonano operacje jedziemy dalej!" << endl;
+         Server1.SendFile();
       }                             
     return 0; 
   }
